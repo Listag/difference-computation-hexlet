55fdae51b497135d945ff4104932364f2944a3e0
Listag <ovseychik.lika@gmail.com>
2022-05-27 15:01:09 +0300

    Добавила аргументы и опции

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,11 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .action(function(filepath1, filepath2) {
     const diff = genDiff(filepath1, filepath2);
     console.log(diff);
-  });
+  })
 
 program.parse();
